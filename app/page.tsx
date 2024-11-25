7db236744691fa985cf4b776d320091d0d8d7d48
n232122321 <120481806+zskizz@users.noreply.github.com>
2024-11-25 22:08:00 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+       Hola! esta es la beta de la web {"  "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
+           todavia no tiene muchas funciones
             </code>
             .
           </li>
